47ca402431534792b9514e0b1b2eef2740e98157
Moritz Schneider <scmoritz@student.ethz.ch>
2018-04-06 16:30:26 +0200

    cleanup and working tests

--- build.rs
+++ build.rs
@@ -4,24 +4,12 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    // Tell cargo to tell rustc to link the system bzip2
-    // shared library.
-    println!("cargo:rustc-link-search=./");
-    println!("cargo:rustc-link-lib=static=noiseprotocol");
-    // println!("cargo:include=./noise/");
-    // println!("cargo:include=./");
-
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
     let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
         .header("noise/protocol.h")
+        .link_static("libnoiseprotocol.a")
         .clang_arg("-I./")
-        // Finish the builder and generate the bindings.
+        .blacklist_type("max_align_t")
         .generate()
-        // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
